90ccf95156db8812728081b67541a0b579eda78f
thuesing <t.huesing@gmx.de>
2014-11-04 15:41:59 +0100

    x axis top labels, cut long node names

--- d3script.js
+++ d3script.js
@@ -1,15 +1,15 @@
-var param = "ZZ_Klima-Land.csv";
+var param = "ZZ_Land-Klima.csv";
 
 // margin convention
 // http://bl.ocks.org/mbostock/3019563 
 // First define the margin object with properties for the four sides (clockwise from the top, as in CSS).
 
-var margin = {top: 30, right: 20, bottom: 300, left: 300};
+var margin = {top: 300, right: 300, bottom: 300, left: 300};
 
 // Then define width and height as the inner dimensions of the chart area.
 // has to be w = h = 650
-var w = 960 - margin.left - margin.right,
-    h = 970 - margin.top - margin.bottom;
+var w = 1200 - margin.left - margin.right,
+    h = 1200 - margin.top - margin.bottom;
 
 // Lastly, define svg as a G element that translates the origin to the top-left corner of the chart area.
 
@@ -28,15 +28,24 @@ var sourcesByName = {}, targetsByName = {}, links = [];
 d3.csv("data/" + param, function(error, data) {
 
     data.forEach(function (d) {
+
         if(d.source != "" && d.target != "" && d.target != "null") { // Warum sind Daten fehlerhaft?
-          // return only the distinct / unique nodes
-          // TODO var source = d.source.substring(0, 10);
           // count sources and targets
-          var source = sourcesByName[d.source] || (sourcesByName[d.source] = {name: d.source, count: 0 });
+          var source = sourcesByName[d.source] || 
+                      (sourcesByName[d.source] = {name: d.source, count: 0 });
               source.count += 1;
-              console.log(source);
-          var target = targetsByName[d.target] || (targetsByName[d.target] = {name: d.target, count: 0 });
+              //sourcesByName[d.source] = source;
+               //console.log(sourcesByName[d.source]);
+               //console.log(sourcesByName["Allg. THG-Emissionsreduktion"]);
+               //console.log(d.source);
+               //console.log(source);
+
+          var target = targetsByName[d.target] || 
+                      (targetsByName[d.target] = {name: d.target, count: 0 });
               target.count += 1;
+              //console.log(targetsByName[d.target]);
+              //targetsByName[d.target] = target;
+
           links.push({ "source": d.source,
                        "target": d.target,
                        "value": parseFloat(d.vuvalue),
@@ -46,13 +55,13 @@ d3.csv("data/" + param, function(error, data) {
         }
     });
 
-console.log(sourcesByName);
 // TODO order by count
 var yNames = d3.map(sourcesByName).keys().sort(d3.ascending);
 var xNames = d3.map(targetsByName).keys().sort(d3.ascending);
 // TODO var nodesCount = nodesNames.length;
+console.log(sourcesByName);
 
-//console.log(nodesNames);
+//console.log(targetsByName);
 
 var x = d3.scale.linear().domain([0, xNames.length - 1]).range([0, w]),
     y = d3.scale.linear().domain([0, yNames.length - 1]).range([0, h]);
@@ -60,39 +69,54 @@ var x = d3.scale.linear().domain([0, xNames.length - 1]).range([0, w]),
 // http://bl.ocks.org/mbostock/3892919  - how to use ticks
 // http://www.d3noob.org/2013/01/adding-grid-lines-to-d3js-graph.html
 
-var xAxis = d3.svg.axis().scale(x).orient("bottom") // tick direction
+var xAxis = d3.svg.axis().scale(x).orient("top") // tick direction
         .ticks(xNames.length) 
-        .tickFormat(function (d, i) {
-            return xNames[i];
+        .tickFormat(function (d, i) {            
+          if(xNames[i].length > 40) {
+            return xNames[i].substring(0, 38) + "..";
+          } else {
+            return xNames[i]
+          }
         }),
     yAxis = d3.svg.axis().scale(y).orient("left")
         .ticks(yNames.length) 
         .tickFormat(function (d, i) {
-            return yNames[i];
+          if(yNames[i].length > 40) {
+            return yNames[i].substring(0, 38) + "..";
+          } else {
+            return yNames[i]
+          }        
         }),
 
-    xGrid = d3.svg.axis().scale(x).orient("top") // double axes for grid
+    xGrid = d3.svg.axis().scale(x).orient("bottom") // double axes for grid
         .ticks(xNames.length)
         .tickSize(-h, 0, 0)
+        .tickFormat(function (d, i) {
+          //console.log(targetsByName[xNames[i]]);
+          //return targetsByName[xNames[i]].count;
+        }) 
        //.tickFormat("")  // hide labels
         ,
     yGrid = d3.svg.axis().scale(y).orient("right")
         .ticks(yNames.length)
         .tickSize(-w, 0, 0)
+        .tickFormat(function (d, i) {
+          //console.log(sourcesByName[yNames[i]]);
+        })
         //.tickFormat("") // hide labels
         ;
 
 // vertical labels - http://bl.ocks.org/mbostock/4403522 
 svg.append("g")  
     .attr("class", "axis")
-    .attr("transform", "translate(0, " + (h) + ")")
     .call(xAxis)
       .selectAll("text")
-      .attr("y", 0)
+      .attr("y", 15)
       .attr("x", 12)        // distance text/axis
-      .attr("dy", "0.5em")  // center text on tick
-      .attr("transform", "rotate(90)")
+      .attr("dy", "-1em")  // center text on tick
+      .attr("transform", "rotate(-75)")
       .style("text-anchor", "start")
+
     ;
  
 svg.append("g")
@@ -104,10 +128,12 @@ svg.append("g")
     ;
 
 svg.append("g")         
-    .attr("class", "grid")    
+    .attr("class", "grid")  
+    .attr("transform", "translate(0, " + (h) + ")")      
     .call(xGrid)
       .selectAll("text")
-      .attr("y", -12)        // distance text/axis
+      .attr("x", 0)
+      .attr("y", 12)        // distance text/axis
     ; 
 
 svg.append("g")         
@@ -127,7 +153,7 @@ var color = d3.scale.linear()
     .domain([-1, 0, 1])
     .range(["green", "yellow", "red"]);
 
-svg.selectAll("circle")
+var circles = svg.selectAll("circle")
     .data(links) // traversed data
     .enter()
     .append("circle")
