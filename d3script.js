3f8b64c808968dd2e4f0fe5c081090541e833869
thuesing <t.huesing@gmx.de>
2015-02-03 18:47:53 +0100

    interactive demo

--- d3script.js
+++ d3script.js
@@ -13,6 +13,28 @@ d3.csv("data/" + datafile, function(error, data) {
     buildLegend();
 }); // d3.csv call
 
+function mouseover(l) {
+  //console.log(p);
+  d3.selectAll(".xAxis text").classed("active", function(d, i) { 
+    //console.log(l);
+    return i == xNames.indexOf(l.target); });
+  d3.selectAll(".yAxis text").classed("active", function(d, i) { 
+    return i == yNames.indexOf(l.source); });
+  d3.select("#status").html(
+  "<ul>" +
+  "<li>" + l.source +
+  "<li>" + l.target +
+  "<li>Pfade: " + l.count +
+  "<li>Positiv: " + l.positiv_prozent  + "%" +
+  "</ul>"
+    );
+}
+
+function mouseout() {
+   d3.selectAll("text").classed("active", false);
+   d3.select("#status").text("");
+}
+
 function buildViz() {
 // Setup SVG Elem width, height
 var nodeDistance = 15; // ABS
@@ -20,7 +42,7 @@ w = xNames.length * nodeDistance;
 h = yNames.length * nodeDistance;
 var svg = setupSvgElement(w,h);
 
-console.log("yNames.length: " + yNames.length);
+console.log("w, h: " + w + ", " + h);
 
 var x = d3.scale.linear().domain([0, xNames.length - 1]).range([0, w]),
     y = d3.scale.linear().domain([0, yNames.length - 1]).range([0, h]);
@@ -82,7 +104,7 @@ if(axesLabels) {
 
 // vertical labels - http://bl.ocks.org/mbostock/4403522 
 svg.append("g")  
-    .attr("class", "axis")
+    .attr("class", "axis xAxis")
     .call(xAxis)
       .selectAll("text")
       .attr("y", 15)
@@ -94,7 +116,7 @@ svg.append("g")
     ;
  
 svg.append("g")
-    .attr("class", "axis")
+    .attr("class", "axis yAxis")
     .call(yAxis)
       .selectAll("text")
       .attr("y", 0)
@@ -155,6 +177,9 @@ var link = svg.selectAll("circle")
     .style("stroke", "white")
     ;
 
+    link.on("mouseover", mouseover);
+    link.on("mouseout", mouseout);
+
     link.append("text").text(
       function(d){
        if(d.positiv > 0)  return (d.positiv_prozent).toString();
@@ -185,16 +210,16 @@ function buildLegend() {
   console.log("Sum Links Count: " +  sumLinkCount);
 
   var legende = d3.select("#legende");
-  //legende.append("h2").text(labels[2] + ", " + labels[3]);
-  // legende.append("h2").text("Dünger-Einsatz - Regulierung, Alle");
   legende.append("div").text("Data: " + datafile);
   legende.append("div").text("Verbindungen gesamt: " + sumLinkCount);
   legende.append("div").text("Max. Verbindungen: " + maxLinkCount);
   legende.append("div").text("Max. verst\u00e4rk. Verbindungen: " + maxLinkPosCount);
   legende.append("div").attr("id","kgreen").text(" 0 verst\u00e4rk. Verbindungen");
   legende.append("div").attr("id","kred").text(" 1 - " + maxLinkPosCount + " verst\u00e4rk. Verbindungen");
-  legende.append("br");
+  //legende.append("br");
   legende.append("div").attr("id","prozente").text("Ziffern i.d. Matrix: Prozent positiv");
+  //legende.append("hr");
+ 
 }
 function parseData(data) { // from d3.csv
 
@@ -234,7 +259,7 @@ function setupSvgElement(width,height) { // based on node count, without margins
 // http://bl.ocks.org/mbostock/3019563 
 // First define the margin object with properties for the four sides (clockwise from the top, as in CSS).
  
-var margin = {top: 300, right: 300, bottom: 300, left: 300};
+var margin = {top: 200, right: 300, bottom: 20, left: 300};
 
 // Then define width and height as the inner dimensions of the chart area.
 
@@ -242,21 +267,30 @@ var margin = {top: 300, right: 300, bottom: 300, left: 300};
 var w = width + margin.left + margin.right,
     h = height + margin.top + margin.bottom;
 
-console.log("w, h from nodecount: " + w + ", " + h);    
+console.log("w, h plus margins: " + w + ", " + h);    
 
 // Lastly, define svg as a G element that translates the origin to the top-left corner of the chart area.
 
+ 
+var svg = d3.select("#chart").append("svg")
+    .attr("width", w )
+    .attr("height", h )
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+ 
+/*
 var svg = d3.select("#chart").append("svg")
     .attr("width", w + margin.left + margin.right)
     .attr("height", h + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+ 
 
+*/
 // With this convention, all subsequent code can ignore margins.
 
 return svg;
 }
-
 
 /* margin convention
 // http://bl.ocks.org/mbostock/3019563 
